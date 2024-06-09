6da89041bf435c38d447ce45a7a2ae5393fa8a43
jaysafdari <123494345+jaysafdari@users.noreply.github.com>
2024-06-08 21:27:24 -0400

    Logic for handle keydown event added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,27 @@ $(document).ready(function(){
 
     // Add initial numbers
     addInitialNumbers();
-})
+
+    // Handle keydown events
+    $(document).keydown(function(event){
+        switch(event.key){
+            case "ArrowUp":
+                moveUp();
+                break;
+            case "ArrowDown":
+                moveDown();
+                break;
+            case "ArrowLeft":
+                moveLeft();
+                break;
+            case "ArrowRight":
+                moveRight();
+                break;
+        }
+
+        addNewNumber();
+    });
+});
 
 function initializeGrid(){
     const gridContainer = $('#grid-container');
@@ -15,8 +35,42 @@ function initializeGrid(){
 }
 
 function addInitialNumbers(){
-    
+
     const randomCell = Math.floor(Math.random() * 9);
 
     $('#cell-' + randomCell).text(2);
+}
+
+function addNewNumber(){
+    const emptyCells = [];
+    for (let i = 0; i < 9; i++) {
+        if ($('#cell-' + i).text() === '') {
+            emptyCells.push(i);
+        }
+    }
+    if(emptyCells.length > 0){
+        const randomCell = Math.floor(Math.random() * emptyCells.length);
+        $('#cell-' + emptyCells[randomCell]).text(2);
+    }
+}
+
+// Movement function
+function moveUp(){
+    console.log("Move Up");
+
+}
+
+function moveDown(){
+    console.log("Move Down");
+
+}
+
+function moveLeft(){
+    console.log("Move Left");
+
+}
+
+function moveRight(){
+    console.log("Move Right");
+
 }
